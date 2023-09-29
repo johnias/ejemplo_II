17c32c69554313df5bd39c84979b52deb80695aa
johnias <johnibarguen@gmail.com>
2023-09-28 20:12:06 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,15 @@
+let numero = -5;
+let palabra = "¡Hola Mundo!";
+let nombre = 'Ana';
+let altura = ' metro y medio';
+console.log(numero);
+let respuesta = false;
+numero = 10;
+const PI = 3.14;
+document.write(altura);
+
+
+console.log(numero);
+console.log(nombre + altura);
+console.log(respuesta);
+alert(nombre);
